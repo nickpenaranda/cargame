09dc4e86171d2711a64c498a7b563892ad94c972
Nick Penaranda <nick4p@gmail.com>
2011-09-28 01:17:36 -0400

    Hard coded my ip address.......

--- src/org/cargame/Client.java
+++ src/org/cargame/Client.java
@@ -6,75 +6,76 @@ import com.captiveimagination.jgn.*;
 import com.captiveimagination.jgn.clientserver.*;
 import com.captiveimagination.jgn.event.*;
 
-
 /**
  * @author Matthew D. Hicks
  */
 public class Client extends DynamicMessageAdapter {
-    private JGNClient client;
-    private int seq = 0;
-    private UpdateMessage newMessage = null;
+  private JGNClient client;
+  private int seq = 0;
+  private UpdateMessage newMessage = null;
 
-	public Client() throws Exception {
-		JGN.register(UpdateMessage.class);
+  public Client() throws Exception {
+    JGN.register(UpdateMessage.class);
 
-		client = new JGNClient();
-		client.addMessageListener(this);
-		//client.addMessageListener(new DebugListener("ChatClient>"));
-		JGN.createThread(1, client).start();
+    client = new JGNClient();
+    client.addMessageListener(this);
+    // client.addMessageListener(new DebugListener("ChatClient>"));
+    JGN.createThread(1, client).start();
 
-		InetSocketAddress reliableServerAddress = new InetSocketAddress(InetAddress.getLocalHost(), 2000);
-		InetSocketAddress fastServerAddress = new InetSocketAddress(InetAddress.getLocalHost(), 2001);
+    InetSocketAddress reliableServerAddress = new InetSocketAddress(InetAddress
+        .getByName("192.168.0.6"), 2000);
+    InetSocketAddress fastServerAddress = new InetSocketAddress(InetAddress
+        .getByName("192.168.0.6"), 2001);
 
-        System.out.println("connecting");
-		client.connectAndWait(reliableServerAddress, fastServerAddress, 1000);
-        System.out.println("connected");
+    System.out.println("connecting");
+    client.connectAndWait(reliableServerAddress, fastServerAddress, 1000);
+    System.out.println("connected");
 
-        UpdateMessage message = new UpdateMessage();
-        message.connecting = true;
-        client.sendToServer(message);
-        waitForPlayers();
-	}
+    UpdateMessage message = new UpdateMessage();
+    message.connecting = true;
+    client.sendToServer(message);
+    waitForPlayers();
+  }
 
-    public void waitForPlayers() throws Exception {
-        UpdateMessage message = nextMessage(0);
-        System.out.println("WAITING");
-        if (message.ready) {
-            System.out.println("READY!");
-        } else {
-            System.out.println("WTF");
-        }
+  public void waitForPlayers() throws Exception {
+    UpdateMessage message = nextMessage(0);
+    System.out.println("WAITING");
+    if (message.ready) {
+      System.out.println("READY!");
+    } else {
+      System.out.println("WTF");
     }
+  }
 
-    public UpdateMessage doUpdate(double x, double y) throws Exception {
-        UpdateMessage message = new UpdateMessage();
-        message.setPlayerId(client.getPlayerId());
-        message.seq = seq;
-        message.x = x;
-        message.y = y;
+  public UpdateMessage doUpdate(double x, double y) throws Exception {
+    UpdateMessage message = new UpdateMessage();
+    message.setPlayerId(client.getPlayerId());
+    message.seq = seq;
+    message.x = x;
+    message.y = y;
 
-        client.broadcast(message);
+    client.broadcast(message);
 
-        UpdateMessage response = nextMessage(seq);
-        seq++;
-        return response;
+    UpdateMessage response = nextMessage(seq);
+    seq++;
+    return response;
+  }
+
+  private UpdateMessage nextMessage(int expected_seq) throws Exception {
+    // validate seq?
+    while (true) {
+      if (newMessage != null) {
+        // System.out.println("got it");
+        UpdateMessage message = newMessage;
+        newMessage = null;
+        return message;
+      }
+      Thread.sleep(1);
     }
+  }
 
-    private UpdateMessage nextMessage(int expected_seq) throws Exception {
-        // validate seq?
-        while (true) {
-            if (newMessage != null) {
-                //System.out.println("got it");
-                UpdateMessage message = newMessage;
-                newMessage = null;
-                return message;
-            }
-            Thread.sleep(1);
-        }
-    }
-
-	public void messageReceived(UpdateMessage message) {
-//		System.out.println("RECEIVED MESSAGE: " + message);
-//        newMessage = message;
-	}
+  public void messageReceived(UpdateMessage message) {
+    // System.out.println("RECEIVED MESSAGE: " + message);
+    // newMessage = message;
+  }
 }
--- src/org/cargame/server/Server.java
+++ src/org/cargame/server/Server.java
@@ -12,68 +12,69 @@ import com.captiveimagination.jgn.*;
 import com.captiveimagination.jgn.clientserver.*;
 import com.captiveimagination.jgn.event.*;
 
-
 public class Server extends DynamicMessageAdapter {
-    private static int NUM_PLAYERS = 2;
-    private boolean[] playersWhoSent = new boolean[NUM_PLAYERS];
-    private int cur_seq = 0;
-    private int connected_players = 0;
+  private static int NUM_PLAYERS = 2;
+  private boolean[] playersWhoSent = new boolean[NUM_PLAYERS];
+  private int cur_seq = 0;
+  private int connected_players = 0;
 
-    Queue<UpdateMessage> prev_messages = new LinkedList<UpdateMessage>();
-    JGNServer server;
+  Queue<UpdateMessage> prev_messages = new LinkedList<UpdateMessage>();
+  JGNServer server;
 
-    public Server() throws Exception {
-        JGN.register(UpdateMessage.class);
-        InetSocketAddress reliableAddress = new InetSocketAddress(InetAddress.getLocalHost(), 2000);
-        InetSocketAddress fastAddress = new InetSocketAddress(InetAddress.getLocalHost(), 2001);
-        server = new JGNServer(reliableAddress, fastAddress);
-        server.addMessageListener(this);
-        JGN.createThread(1, server).start();
+  public Server() throws Exception {
+    InetAddress HOST_ADDRESS = InetAddress.getByName("192.168.0.6");
+    JGN.register(UpdateMessage.class);
+    InetSocketAddress reliableAddress = new InetSocketAddress(HOST_ADDRESS,
+        2000);
+    InetSocketAddress fastAddress = new InetSocketAddress(HOST_ADDRESS, 2001);
+    server = new JGNServer(reliableAddress, fastAddress);
+    server.addMessageListener(this);
+    JGN.createThread(1, server).start();
+  }
+
+  public void connected() {
+    System.out.println("CONNECTED");
+    connected_players++;
+    if (connected_players == NUM_PLAYERS) {
+      UpdateMessage message = new UpdateMessage();
+      message.ready = true;
+      server.sendToAll(message);
     }
+  }
 
-    public void connected() {
-        System.out.println("CONNECTED");
-        connected_players++;
-        if (connected_players == NUM_PLAYERS) {
-            UpdateMessage message = new UpdateMessage();
-            message.ready = true;
-            server.sendToAll(message);
-        }
+  private void validateMessage(UpdateMessage message) {
+    // TODO: validate player isn't sending the same seq again
+    if (message.seq != cur_seq) {
+      assert (allPlayersSent());
+      assert (cur_seq + 1 == message.seq);
+      cur_seq++;
+      clearPlayersWhoSent();
     }
+  }
 
-    private void validateMessage(UpdateMessage message) {
-        // TODO: validate player isn't sending the same seq again
-        if (message.seq != cur_seq) {
-            assert (allPlayersSent());
-            assert (cur_seq + 1 == message.seq);
-            cur_seq++;
-            clearPlayersWhoSent();
-        }
-    }
+  private boolean allPlayersSent() {
+    for (int i = 0; i < playersWhoSent.length; i++)
+      if (!playersWhoSent[i])
+        return false;
+    return true;
+  }
 
-    private boolean allPlayersSent() {
-        for (int i=0; i<playersWhoSent.length; i++)
-            if (!playersWhoSent[i])
-                return false;
-        return true;
-    }
+  private void clearPlayersWhoSent() {
+    for (int i = 0; i < playersWhoSent.length; i++)
+      playersWhoSent[i] = false;
+  }
 
-    private void clearPlayersWhoSent() {
-        for (int i=0; i<playersWhoSent.length; i++)
-            playersWhoSent[i] = false;
+  public void messageReceived(UpdateMessage message) {
+    if (message.connecting) {
+      connected();
+    } else {
+      validateMessage(message);
+      System.out.println("MESSAGE: " + message);
+      validateMessage(message);
     }
+  }
 
-    public void messageReceived(UpdateMessage message) {
-        if (message.connecting) {
-            connected();
-        } else {
-            validateMessage(message);
-            System.out.println("MESSAGE: " + message);
-            validateMessage(message);
-        }
-    }
-
-    public static void main(String[] args) throws Exception {
-        new Server();
-    }
+  public static void main(String[] args) throws Exception {
+    new Server();
+  }
 }
