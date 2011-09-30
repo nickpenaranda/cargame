a034c5360967abebd87ff20f90eb3ea3fe5816e7
Nick Penaranda <nick4p@gmail.com>
2011-09-30 14:52:48 -0400

    Better explosions

--- src/org/cargame/Explosion.java
+++ src/org/cargame/Explosion.java
@@ -1,37 +1,46 @@
 package org.cargame;
 
+import java.util.Random;
+
 import org.newdawn.slick.Color;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.SpriteSheet;
 
 public class Explosion {
+  private static final Random r = new Random();
   private static final int EXPLOSION_DURATION = 1000;
   public double x,y;
   public int life;
+  private int variant;
   
   public Explosion(double x,double y) {
     this.x = x;
     this.y = y;
     life = EXPLOSION_DURATION;
+    variant = r.nextInt(4);
   }
   
   public Image getImage() {
-    return(getFrame((int)(16 * (1 - (life/(float)EXPLOSION_DURATION)))));
+    return(getFrame(variant,(int)(16 * (1 - (life/(float)EXPLOSION_DURATION)))));
   }
   
-  private static SpriteSheet mSpriteSheet;
+  private static SpriteSheet[] mSpriteSheet;
   
   public static void init() {
+    mSpriteSheet = new SpriteSheet[4];
+    
     try {
-      mSpriteSheet = new SpriteSheet("gfx/explode1.png",64,64,Color.magenta);
+      for(int i=0;i<4;i++) {
+        mSpriteSheet[i] = new SpriteSheet("gfx/explode" + (i + 1) + ".png",64,64,Color.magenta);
+      }
     } catch (SlickException e) {
       // TODO Auto-generated catch block
       e.printStackTrace();
     }
   }
   
-  private static Image getFrame(int frame) {
-    return mSpriteSheet.getSprite(frame % 4, frame / 4);
+  private static Image getFrame(int variant, int frame) {
+    return mSpriteSheet[variant].getSprite(frame % 4, frame / 4);
   }
 }
