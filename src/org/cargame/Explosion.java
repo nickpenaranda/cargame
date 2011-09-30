02de72edff5e1486880d0578a13ef8eb239f8c21
Nick Penaranda <nick4p@gmail.com>
2011-09-30 17:35:34 -0400

    Explosions refined

--- src/org/cargame/Explosion.java
+++ src/org/cargame/Explosion.java
@@ -10,6 +10,7 @@ import org.newdawn.slick.SpriteSheet;
 public class Explosion {
   private static final Random r = new Random();
   private static final int EXPLOSION_DURATION = 1000;
+  private static final int NUM_VARIANTS = 5;
   public double x,y;
   public int life;
   private int variant;
@@ -18,7 +19,7 @@ public class Explosion {
     this.x = x;
     this.y = y;
     life = EXPLOSION_DURATION;
-    variant = r.nextInt(4);
+    variant = r.nextInt(NUM_VARIANTS);
   }
   
   public Image getImage() {
@@ -28,10 +29,10 @@ public class Explosion {
   private static SpriteSheet[] mSpriteSheet;
   
   public static void init() {
-    mSpriteSheet = new SpriteSheet[4];
+    mSpriteSheet = new SpriteSheet[NUM_VARIANTS];
     
     try {
-      for(int i=0;i<4;i++) {
+      for(int i=0;i<NUM_VARIANTS;i++) {
         mSpriteSheet[i] = new SpriteSheet("gfx/explode" + (i + 1) + ".png",64,64,Color.magenta);
       }
     } catch (SlickException e) {
