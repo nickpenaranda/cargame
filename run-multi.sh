7c3b492cfd3269343dce27d37ccc294df675d847
Nick Penaranda <nick4p@gmail.com>
2011-09-30 12:46:16 -0400

    Updated run-multi.sh

--- run-multi.sh
+++ run-multi.sh
@@ -3,5 +3,7 @@
 ./compile.sh && java \
     -Djava.library.path=lib/native \
     -Dcargame.multiplayer_mode=true \
+    -Dcargame.host_name="intis.dyndns.org" \
+    -Dcargame.player_name="Shawn" \
     -classpath lib/lwjgl.jar:lib/slick.jar:src:lib/JGN_20110725.jar \
     org.cargame.CarGame
