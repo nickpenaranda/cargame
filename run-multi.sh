8012992f45cc3f46a41169e899dc284bf7a89811
Shawn Lewis <shlewis@gmail.com>
2014-03-11 13:37:52 -0700

    run-multi uses command line args for name, server ip

--- run-multi.sh
+++ run-multi.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+if [ -z $2 ]; then
+    echo "Usage: ./run-multi.sh <player_name> <server_ip>"
+    exit 1;
+fi
+
 ./compile.sh && java \
     -Djava.library.path=lib/native \
     -Dcargame.multiplayer_mode=true \
-    -Dcargame.host_name=76.21.9.224 \
-    -Dcargame.player_name=shawn \
+    -Dcargame.host_name=$2 \
+    -Dcargame.player_name=$1 \
     -classpath lib/lwjgl.jar:lib/slick.jar:src:lib/kryonet-1.04-all.jar \
     org.cargame.CarGame
